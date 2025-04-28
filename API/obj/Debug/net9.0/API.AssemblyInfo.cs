12d457ed9cfdef198a9e7fcd6bf32d976f49a742
soflatechie <ginzo.web@gmail.com>
2025-04-28 10:00:14 -0400

    chnages

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e98ac28b4f2b9d2786435faceb63b5599bd2189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b430e6d17f2abf329ef5305668624b8e4b331b44")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
